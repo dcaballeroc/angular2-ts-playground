5d9c8bb21d776a2ae933e669eb69454d851f9c26
Danilo Caballero <fabrizio.dfc2@gmail.com>
2016-06-10 11:27:27 -0700

    Added '@angular/http' and 'angular2-in-memory-web-api'. Replaced 'heroes.mock' with 'inMemoryDataService'. 'getHeroes' uses Http.

--- src/app/shared/hero.service.ts
+++ src/app/shared/hero.service.ts
@@ -1,21 +1,29 @@
 import { Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http';
 
 import { Hero } from './';
-import { HEROES } from './heroes.mock';
 
 @Injectable()
 export class HeroService {
-  getHeroes() {
-    return Promise.resolve(HEROES);
+  private heroesUrl = 'api/heroes';
+
+  constructor(private http: Http){
   }
 
-  getHeroesSlowly() {
-    return new Promise<Hero[]>(resolve =>
-      setTimeout(() => resolve(HEROES), 5000)
-    );
+  getHeroes(): Promise<Hero[]> {
+    return this.http.get(this.heroesUrl)
+      .toPromise()
+      .then(response => response.json().data)
+      .catch(this.handleError);
   }
 
   getHero(id: number) {
     return this.getHeroes().then(heroes => heroes.filter(hero => hero.id === id)[0]);
+  }
+
+  private handleError(error: any) {
+    console.error('An error occurred: ', error);
+
+    return Promise.reject(error.message || error);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
+import { provide } from '@angular/core';
+import { HTTP_PROVIDERS, XHRBackend } from '@angular/http';
 import { bootstrap } from '@angular/platform-browser-dynamic';
+import { InMemoryBackendService, SEED_DATA } from 'angular2-in-memory-web-api';
+
 import { AppComponent } from './app/app.component';
+import { InMemoryDataService } from './app/shared';
 
 import './main.scss';
 
-bootstrap(AppComponent);
+bootstrap(AppComponent, [
+  HTTP_PROVIDERS,
+  provide(XHRBackend, { useClass: InMemoryBackendService }),
+  provide(SEED_DATA, { useClass: InMemoryDataService }),
+]);
