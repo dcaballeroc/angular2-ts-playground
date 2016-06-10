831794e2612ad187982cd7eaefd1466c4d447aff
Danilo Caballero <fabrizio.dfc2@gmail.com>
2016-06-10 11:46:07 -0700

    Added CRUD methods to HeroService

--- src/app/shared/hero.service.ts
+++ src/app/shared/hero.service.ts
@@ -17,8 +17,43 @@ export class HeroService {
       .catch(this.handleError);
   }
 
-  getHero(id: number) {
+  getHero(id: number): Promise<Hero> {
     return this.getHeroes().then(heroes => heroes.filter(hero => hero.id === id)[0]);
+  }
+
+  save(hero: Hero): Promise<Hero> {
+    if (hero.id)
+      return this.put(hero);
+
+    return this.post(hero);
+  }
+
+  delete(hero: Hero) {
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let url = `${this.heroesUrl}/${hero.id}`;
+
+    return this.http.delete(url, { headers })
+      .toPromise()
+      .catch(this.handleError);
+  }
+
+  private post(hero: Hero): Promise<Hero> {
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+
+    return this.http.post(this.heroesUrl, JSON.stringify(hero), { headers })
+      .toPromise()
+      .then(response => response.json().data)
+      .catch(this.handleError);
+  }
+
+  private put(hero: Hero): Promise<Hero> {
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let url = `${this.heroesUrl}/${hero.id}`;
+
+    return this.http.put(url, JSON.stringify(hero), { headers })
+      .toPromise()
+      .then(() => hero)
+      .catch(this.handleError);
   }
 
   private handleError(error: any) {
